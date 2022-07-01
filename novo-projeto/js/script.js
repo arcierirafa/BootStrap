e70ae6ac4b824d30464b07bd91d524da3d2aa7db
Rafael Arcieri <arcierirafael@gmail.com>
2022-06-30 21:42:35 -0300

    Novo Projeto 30-06-22

--- novo-projeto/js/script.js
+++ novo-projeto/js/script.js
@@ -37,14 +37,27 @@ function validarFormulario (){
 	console.log (caixaTexto)
 
 }
-function mascaraDeTelefone(telefone){
-    const textoAtual = telefone.value;
-    const isCelular = textoAtual.length === 9;
-    let textoAjustado;
-        if(isCelular) {
-         // faz alguma coisa 
-        } else {
-         // faz alguma coisa
-        }
-    telefone.value = textoAjustado;
+function mascara(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout("execmascara()",1)
 }
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value)
+}
+function mtel(v){
+    v=v.replace(/\D/g,""); //Remove tudo o que não é dígito
+    v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
+    v=v.replace(/(\d)(\d{4})$/,"$1-$2"); //Coloca hífen entre o quarto e o quinto dígitos
+    return v;
+}
+function id( el ){
+	return document.getElementById( el );
+}
+window.onload = function(){
+	id('inputFone').onkeyup = function(){
+		mascara( this, mtel );
+	}
+}
+
+ 
